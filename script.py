72c085096ba58d585090773757757ea529a36e24
Vallee Carey <60826335+ValleeCarey@users.noreply.github.com>
2020-08-24 00:12:44 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,20 @@
+toppings = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'olives', 'anchovies', 'mushrooms']
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_pizzas = len(toppings)
+
+#print('We sell ' + str(num_pizzas) + ' different kinds of pizza!')
+
+pizzas = list(zip(prices, toppings))
+#print(pizzas)
+
+cheapest_pizza = pizzas.sort()
+
+priciest_pizza = pizzas[-1]
+
+three_cheapest = pizzas[0:3]
+print(three_cheapest)
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
